8957b79977ab60e834959d08112ddb6781612ee9
loganrod <70386750+loganrod@users.noreply.github.com>
2020-11-09 14:48:04 -0500

    Update EmailVerification.js

--- frontend/src/components/EmailVerification.js
+++ frontend/src/components/EmailVerification.js
@@ -3,14 +3,29 @@ const nodemailer = require('nodemailer');
 
 function verify()
 {
+    const app_name = 'group9-meetingscheduler';
+    function buildPath(route)
+    {
+        if (process.env.NODE_ENV === 'production') 
+        {
+            return 'https://' + app_name +  '.herokuapp.com/' + route;
+        }
+        else
+        {        
+            return 'http://localhost:5000/' + route;
+        }
+    }
+    
+    var email;
+    
     let transporter = nodemailer.createTransport
     ({
         service: 'gmail'
         auth:
         {
             user: 'group9scheduler@gmail.com',
-            pass:'cop4331$',
-        },
+            pass:'cop4331$'
+        }
     });
 
 
@@ -20,21 +35,34 @@ function verify()
         to: 'loganrod16@gmail.com',
         subject: 'Email Verification',
         text: 'TEST'
-    }
+    };
     
-    function doVerify()
+    const doVerify = async event => 
     {
-        transporter.sendMail( mail, function( error, data ) )
+        event.preventDefault();
+        
+        var obj = { email:email.value }
+        var js = JSON.stringify(obj);
+        
+        try
         {
-        if( error == true )
-        {
-            console.log('An error has occured');
-        }
-        else
-        {
-            console.log('The mail was sent!');
-        }
+            // Continue editing here
+            const response = await fetch(buildPath('api/createUser'),
+            {method:'POST',body:js,headers:{'Content-Type':'application/json'}});
+            var txt = await response.text();
+            var res = JSON.parse(txt);
 
+            if(res.error.length > 0)
+            {
+                setMessage("API Error:" + res.error);
+            }
+            else
+            {
+                setMessage('User has been added');
+            }
+        }
+        catch(e)
+        {
+            setMessage(e.toString());
+        }
     }
-    
-}
