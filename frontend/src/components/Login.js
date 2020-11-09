9364e56301e260ad81b65021b8bee22f77160c35
loganrod <70386750+loganrod@users.noreply.github.com>
2020-11-09 14:21:11 -0500

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -54,7 +54,7 @@ function Login()
     return(
         <div id="loginDiv">
             <form onSubmit={doLogin}>
-                <span id ="inner-title">Please Login</span><br />
+                <span id ="inner-title">Please Login!</span><br />
                 <input type="text" id="loginName" placeholder="Username" ref={ (c) => loginName = c} /><br />
                 <input type="password" id="loginPassword" placeholder="Password" ref={ (c) => loginPassword = c} /><br />
                 <input type="submit" id="loginButton" class="buttons" value="Login" onClick={doLogin} />
